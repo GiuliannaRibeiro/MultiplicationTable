c6e69bc299806748a297725e0ac04fb01eedbe69
Giulianna Ribeiro <82107688+GiuliannaRibeiro@users.noreply.github.com>
2022-07-01 16:37:23 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,14 +4,14 @@ function tabuada(){
     if (num.value == 0){
         alert('Por favor, Digite um número!')
     } else{
-        let n = Number(num.value) //convertendo os números que estão em formato de strings do 'num' em números.
+        let n = Number(num.value)
         let c = 1
-        tab.innerHTML = '' //antes de começar a mostrar a tabuada, a área de tabuada sempre vai ser limpada.
+        tab.innerHTML = '' //Limpar a área da tabuada.
         while (c <= 10) {
-            let item = document.createElement('option') //criando um elemento option, porque para colocar opções dentro do select, tem que haver options.
-            item.value = `tab${c}`//nas options sempre tem que ter um value, para não dar problema na linguagem php.(mas para o JS não faz tanta diferença.)
-            item.text = `${n} x ${c} = ${n*c}`//item.text = parte de dentro do option.
-            tab.appendChild(item)  //para fazer a conta acima aparecer, tem que selecionar o elemento "tab" (que é a tabuada) .appendchild (adicionar um elemento filho) que vai ser o "item".
+            let item = document.createElement('option') 
+            item.value = `tab${c}`
+            item.text = `${n} x ${c} = ${n*c}`
+            tab.appendChild(item)  
             c++
         }
     }
